e64fc13e78b58786c4d30e86daa453c351a1551b
Simone Mauro <simon.mauro@virgilio.it>
2022-06-30 14:48:12 +0200

    Aggiungere i file di progetto.

---
+++ Sistema.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Sistema{
+        List<Utente> utentiRegistrati = new List<Utente>();
+        List<Documento> documentiPresenti = new List<Documento> {
+            new Libro("Moby Dick", 1857, "romanzo", true, 21, "bho", "bho2", 97888123, 1200),
+            new DVD("test", 1921, "musica jazz", true, 115, "Led", "zeppelin", 4568478, 3)
+        };
+
+        public void registraNuovoUtente(Utente nuovoUtente){
+            bool utenteValido = nuovoUtente.nome != null && nuovoUtente.cognome != null && nuovoUtente.email != null && nuovoUtente.numeroTelefonico != null;
+
+            if (utenteValido){
+                this.utentiRegistrati.Add(nuovoUtente);
+            }
+        }
+    }
+
+    
+}
---
+++ Utente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Utente{
+        public string cognome;
+        public string nome;
+        public string email;
+        private string password;
+        public string numeroTelefonico;
+    }
+}
